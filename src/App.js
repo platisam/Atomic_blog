204a409d2958499342e3cec679bdfb8e63a716aa
platisam <platisam@gmail.com>
2023-07-14 22:52:00 +0200

    creating function for random posts

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
 function App() {
   return <div className="App">Atomic_blog</div>;
 }
