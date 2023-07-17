ecab05e497f3cf9e3058da21c8f3e9fb5631f1a8
platisam <platisam@gmail.com>
2023-07-17 12:07:34 +0200

    setting state in Archive component

--- src/Archive.js
+++ src/Archive.js
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
 const Archive = ({ onAddPost }) => {
+  const [posts] = useState(() =>
+    Array.from({ length: 100 }, () => createRandomPost())
+  );
+
+  const [showArchive, setShowArchive] = useState(false);
+
   return <aside></aside>;
 };
 export default Archive;
