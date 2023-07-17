62115f65e60b3457f55aef235e6c83616b294a7a
platisam <platisam@gmail.com>
2023-07-17 12:24:06 +0200

    displaying archived posts and setting button for adding posts

--- src/Archive.js
+++ src/Archive.js
@@ -15,6 +15,26 @@ const Archive = ({ onAddPost }) => {
 
   const [showArchive, setShowArchive] = useState(false);
 
-  return <aside></aside>;
+  return (
+    <aside>
+      <h2>Post archive</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
 };
 export default Archive;
