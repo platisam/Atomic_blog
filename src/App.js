8c70b77be9fa2be6b931a536c306e401d31fca6f
platisam <platisam@gmail.com>
2023-07-18 12:13:22 +0200

    added PostContext custom provider and refactoring code

--- src/App.js
+++ src/App.js
@@ -14,62 +14,26 @@ function createRandomPost() {
   };
 }
 
-const PostContext = createContext();
-
 function App() {
-  const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
-  );
-
-  const [searchQuery, setSearchQuery] = useState("");
-  //const [isFakeDark, setIsFakeDark] = useState(false);
-
-  const searchedPosts =
-    searchQuery.length > 0
-      ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
-        )
-      : posts;
-
-  function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
-  }
-
-  function handleClearPosts() {
-    setPosts([]);
-  }
-
   /*useEffect(() => {
     document.documentElement.classList.toggle("fake-dark-mode");
   }, [isFakeDark]);*/
 
   return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      <section>
-        {/*<button
+    <section>
+      {/*<button
           onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
           className="btn-fake-dark-mode"
         >
           {isFakeDark ? "☀️" : "🌙"}
     </button>*/}
-        <Button />
+      <Button />
 
-        <Header />
-        <Main />
-        <Archive />
-        <Footer />
-      </section>
-    </PostContext.Provider>
+      <Header />
+      <Main />
+      <Archive />
+      <Footer />
+    </section>
   );
 }
 
