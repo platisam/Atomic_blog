3c87f7fab1126d7c9d38ccd3809378285a793ed8
platisam <platisam@gmail.com>
2023-07-20 10:58:09 +0200

    setting optimization trick with children in Test component

--- src/Test.js
+++ src/Test.js
@@ -1,13 +1,23 @@
 import { useState } from "react";
 
 export default function Test() {
-  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <Counter>
+        <SlowComponent />
+      </Counter>
+    </div>
+  );
+}
 
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase {count}</button>
-      <SlowComponent />
+      {children}
     </div>
   );
 }
