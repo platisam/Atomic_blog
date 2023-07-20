05b14bc6b923741f6982f36dc96bf3ef72bb80ac
platisam <platisam@gmail.com>
2023-07-20 10:36:56 +0200

    included Test component in List

--- src/List.js
+++ src/List.js
@@ -1,17 +1,21 @@
 import { usePosts } from "./PostContext";
+import Test from "./Test";
 
 const List = () => {
   const { posts } = usePosts();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      <Test />
+    </>
   );
 };
 export default List;
