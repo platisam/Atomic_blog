24bc0e5dd841e8170c11adc5d03f5d522c5701eb
platisam <platisam@gmail.com>
2023-07-20 10:15:42 +0200

    created SlowComponent in Test component

--- src/Test.js
+++ src/Test.js
@@ -7,6 +7,20 @@ export default function Test() {
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase {count}</button>
+      <SlowComponent />
     </div>
+  );
+}
+
+function SlowComponent() {
+  const words = Array.from({ length: 1000 }, () => "WORD");
+  return (
+    <ul>
+      {words.map((word, i) => (
+        <li key={i}>
+          {i}: {word}
+        </li>
+      ))}
+    </ul>
   );
 }
