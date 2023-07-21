e544a2a1964954604923850d7a78e0938439eb7a
platisam <platisam@gmail.com>
2023-07-21 11:58:29 +0200

    adding useMemo hook to value object

--- src/PostContext.js
+++ src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 const PostContext = createContext();
@@ -24,26 +24,26 @@ function PostProvider({ children }) {
             .includes(searchQuery.toLowerCase())
         )
       : posts;
+
   function handleAddPost(post) {
     setPosts((posts) => [post, ...posts]);
   }
+
   function handleClearPosts() {
     setPosts([]);
   }
 
-  return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      {children}
-    </PostContext.Provider>
-  );
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchedPosts, searchQuery]);
+
+  return <PostContext.Provider value={value}>{children}</PostContext.Provider>;
 }
 
 function usePosts() {
